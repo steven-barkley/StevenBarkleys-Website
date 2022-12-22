58921ac37f2c0abba65ca71222dffd5d93f273d1
Steven Barkley <steven.barkley@outlook.com>
2022-12-22 15:45:18 -0500

    Commit 6 learning node.js

---
+++ hello.js
@@ -0,0 +1,7 @@
+const mission = process.argv[ 2 ];
+
+if ( mission === 'learning' ) {
+    console.log( 'Time to write some Node code!' );
+} else {
+    console.log( `Is ${ mission } really more fun?` );
+}
