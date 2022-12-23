97b75e7aba4ed6a427faefd7e72bad203adfc40e
Steven Barkley <steven.barkley@outlook.com>
2022-12-22 22:05:37 -0500

    Commit 8

--- hello.js
+++ hello.js
@@ -6,3 +6,9 @@ if ( mission === 'learning' ) {
     console.log( `Is ${ mission } really more fun?` );
 }
 
+//2 
+
+Promise.resolve( 'Data that was requested' ).then( ( data => console.log( '2', data ) ) )
+
+//3
+console.log( '3', 'is a crowd' )
---
+++ promises.js
@@ -0,0 +1,9 @@
+//Why use promises
+
+// A promise is an object that may produce a single value some time in the future
+/* 
+promisedAction( "A value", "A string" )
+    .then( () => promisedAction( "A different value", "A different string" ) )
+    .then( () => promisedAction( "A different value", "A different string" ) )
+    .then( () => promisedAction( "A different value", "A different string" ) ) */
+
